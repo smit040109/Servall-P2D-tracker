aea6300e7e62db4d477d47ac9217c5fd27b5367b
Smit Patel <smit63371@gmail.com>
2025-12-24 16:42:46 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
